1a472426179a47fa96f8bc165df33505b167f62b
Mairon <maironjoelkin430@gmail.com>
2026-09-07 12:47:27 -0600

    style: load admin store workspace styles

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,7 @@ import "./components/product/store-effects.css";
 import "./components/product/cosmetic-identity.css";
 import "./components/product/profile-klipy.css";
 import "./components/admin/admin.css";
+import "./components/admin/store/admin-store.css";
 
 // fallow-ignore-next-line complexity -- route loader combines request context and session recovery.
 export async function loader({ request, context }: ServerLoaderArgs) {
