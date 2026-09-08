37a77049243dafd581abf2c230387b92ca398158
Mairon <maironjoelkin430@gmail.com>
2026-09-08 14:14:51 -0600

    style: load post layout polish

--- app/root.tsx
+++ app/root.tsx
@@ -40,6 +40,7 @@ import "./components/product/profile-summary.css";
 import "./components/product/mobile-product-polish.css";
 import "./components/product/profile-layout-polish.css";
 import "./components/product/store-mobile-polish.css";
+import "./components/product/post-layout-polish.css";
 
 // fallow-ignore-next-line complexity -- route loader combines request context and session recovery.
 export async function loader({ request, context }: ServerLoaderArgs) {
