f242eb930979aaf38018c5aa0591c43fd3a07076
Mairon <maironjoelkin430@gmail.com>
2026-09-10 21:14:03 -0600

    fix(posts): explicitly backfill legacy categories

--- migrations/0028_post_categories.sql
+++ migrations/0028_post_categories.sql
@@ -1,4 +1,7 @@
 ALTER TABLE posts ADD COLUMN category_slug TEXT NOT NULL DEFAULT 'other';
 
+UPDATE posts
+SET category_slug = 'other';
+
 CREATE INDEX posts_category_created_idx
 ON posts (category_slug, created_at DESC, id DESC);
